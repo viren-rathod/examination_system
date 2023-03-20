ef56ad5f95ab1d68085950314904024102124627
skrathod-07 <sandipsinh.rathod.23.esparkbiz@gmail.com>
2023-03-20 19:01:50 +0530

    23DEV055 ejs file updated

--- categories.js
+++ categories.js
@@ -1,242 +1,59 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+const bodyparser = require('body-parser')
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 
 const conn = mysql.createConnection({
 
         host:'localhost',
         user:'root',
         password:'root',
-        database:'student'
+        database:'exam_system'
 });
 
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
 
-    let cat_1=[
-        {
-            'que':'1Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
+    let exam_cat=[];
+    let q_cat =`SELECT exam_system.category.category_name , exam_system.exam_category.exam_id
+    FROM exam_system.category 
+    inner join exam_system.exam_category 
+    on exam_system.category.category_id=exam_system.exam_category.category_id;`;
+    conn.query(q_cat, (err, result) => {
+        if (err) throw err;
 
-        {
-            'que':'2Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        }
-    ];
-    let cat_2=[
-        {
-            'que':'22Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        }
-    ];
-    let cat_3=[
-        {
-            'que':'33Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        }
-    ];
-    let cat_4=[
-        {
-            'que':'44Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        { 
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        },
-
-        {
-            'que':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis tempora, nam voluptatum quam suscipit sapiente laboriosam culpa repellat animi veniam adipisci accusantium enim quisquam quo placeat fugit? Asperiores, voluptate.',
-            'A':'lorem i',
-            'B':'lorem i',
-            'C':'lorem i',
-            'D':'lorem i',
-        }
-    ];
+        exam_cat = result;
+        
+    });
    
-    res.render('categories',{cat_1,cat_2,cat_3,cat_4});
+    let cat_1_que=[];
+    let cat1_que =`select * from questions where category_id='1' `;
+    conn.query(cat1_que, (err, result) => {
+        if (err) throw err;
+        cat_1_que=result;
+        console.log(cat_1_que);
+    });
+
+    let cat_2_que=[];
+    let cat2_que=`select * from questions where category_id='2' `;
+    conn.query(cat2_que, (err, result) => {
+        if (err) throw err;
+        cat_2_que=result;
+        console.log(cat_2_que);
+    });
+
+    let cat_3_que=[];
+    let cat3_que =`select * from questions where category_id='3' `;
+    conn.query(cat3_que, (err, result) => {
+        if (err) throw err;
+        cat_3_que=result;
+        console.log(cat_3_que);
+        res.render('categories',{exam_cat,cat_1_que,cat_2_que,cat_3_que});
+    });
 })
 
 
-app.listen(6001,()=>{console.log('running on port 6001...')});
+app.listen(6005,()=>{console.log('running on port 6005...')});
