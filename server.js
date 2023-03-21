2ee25ed4ec90b3d984146200103c9b39779e49e3
Jaydip Bhetariya <bhetariyajaydip12@gmail.com>
2023-03-21 16:11:21 +0530

    23DEV076-implement form validation and verify access code

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 
-
-var sessionstorage = require('sessionstorage');
+var sessionstorage = require("sessionstorage");
 const body_parser = require("body-parser");
 const path = require("path");
 const mysql = require("mysql2");
@@ -26,36 +25,20 @@ app.set("view engine", "ejs");
 //examlist
 app.get("/", (req, res) => {
   try {
-    const page = req.query;
-    console.log(page);
-    
-    const numberOfperpage = 5;
-    let numberOfResult;
-    let numberOfpage;
-    connection.query('select * from exam', (err, dataresult) => {
-      numberOfResult = dataresult.length;
-      numberOfpage = Math.ceil(numberOfResult/numberOfperpage);
-      let page = req.query.page? Number(req.query.page) : 1;
-      // console.log(page);
-      const startingLimit = (page-1)*numberOfperpage;
-      // console.log(startingLimit);
-      // console.log(numberOfperpage);
-      let sql = 'select * from exam LIMIT '+ startingLimit +","+numberOfperpage;
-      connection.query(sql, (err,result) => {
-        numberOfpage = Math.ceil(numberOfResult/numberOfperpage);
-        // console.log(numberOfpage);
-      res.render("examlist", { examlist: result,numberOfpage,page,numberOfperpage });
+    connection.query("select exam_id, exam_name,exam_time, exam_status from exam", (err,result) => {
+      res.render("examlist", {
+        result: result,
       });
     });
   } catch (err) {
     console.log(err);
   }
 });
+        
       
 
 //student data
 app.get("/studentdata", (req, res) => {
-  
   let studentdata = "select * from user_login  WHERE user_id = 1";
   connection.query(studentdata, (err, result) => {
     if (err) throw err;
@@ -64,20 +47,16 @@ app.get("/studentdata", (req, res) => {
   });
 });
 
-
-
 //exam data
 app.get("/examdata", (req, res) => {
   const id = req.query.id;
-  console.log("id" ,id); 
+  console.log("id", id);
   let examdata = `select * from exam WHERE exam_id = ${id}`;
   connection.query(examdata, (err, examresult) => {
     if (err) throw err;
     res.send(examresult);
   });
-
 });
-
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
