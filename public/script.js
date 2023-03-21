eae696052b92fc8bd324205955efa31899eebe53
viren-rathod <viren.rathod.2023@gmail.com>
2023-03-21 19:24:52 +0530

    23DEV019 : implemented category section

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 async function fetcher(str) {
   let temp = await fetch(str);
   let ans = await temp.json();
-  let s = `<div id="que">
-              <div class="d-flex flex-row align-items-center question-title">
+  // let ans = answer.data;
+  console.log(ans);
+  let s = ` <div class="d-flex flex-row align-items-center question-title">
                 <h3 class="text-danger">Q.</h3>
                 <h5 class="mt-1 ml-2">${ans[0].question_text}</h5>
               </div>
@@ -29,26 +30,49 @@ async function fetcher(str) {
                   <input type="radio" name="q-${ans[0].question_id}" value="${ans[0].option_d}">
                   <span>${ans[0].option_d}</span>
                 </label>
-              </div>
-            </div>`;
+              </div>`;
   que.innerHTML = s;
   let qn = `<span class= "que-no">${ans[0].question_id}</span>`;
   que_no.innerHTML = qn;
-}
-
-async function next_btn(id) {
-  let temp1 = await fetch(`http://localhost:4321/next?id=${id}`);
-  let tmp = await temp1.json();
-  await fetcher(`/paging/?question_no=${tmp[0].question_id}`);
-  let s = `<div class="buttons p-3 bg-white">
-  <div
+  let btn = `<div
     class="row justify-content-around align-items-center"
     id="row"
   >
     <input
       type="button"
       value="PREV"
-      onclick="fetcher('${tmp[0].question_id}')"
+      onclick="previous_btn('${ans[0].question_id}')"
+      class="border border-info rounded p-1 bg-white text-info font-weight-bold col-2"
+      id="prev"
+    />
+
+    <input
+      type="button"
+      value="NEXT"
+      onclick="next_btn('${ans[0].question_id}')"
+      class="btn btn-primary btn-success col-2 font-weight-bold"
+      id="next"
+    />
+  </div>`;
+  btns.innerHTML = btn;
+}
+
+async function next_btn(id) {
+  // console.log(id);
+  let temp1 = await fetch(`/next?id=${id}`);
+  let tmp = await temp1.json();
+  // console.log(tmp[0].question_id);
+  await fetcher(
+    `/paging/?question_no=${tmp[0].question_id}&category_id=${tmp[0].category_id}`
+  );
+  let s = `<div
+    class="row justify-content-around align-items-center"
+    id="row"
+  >
+    <input
+      type="button"
+      value="PREV"
+      onclick="previous_btn('${tmp[0].question_id}')"
       class="border border-info rounded p-1 bg-white text-info font-weight-bold col-2"
       id="prev"
     />
@@ -60,43 +84,114 @@ async function next_btn(id) {
       class="btn btn-primary btn-success col-2 font-weight-bold"
       id="next"
     />
-  </div>
-</div>`;
+  </div>`;
   btns.innerHTML = s;
 }
 
-// async function prev_btn(id) {
-//   let temp2 = await fetch(`http://localhost:4321/prev?id=${id}`);
-//   console.log(temp2);
-  // let tmp = await temp2.json();
-//   await fetcher(`/paging/?question_no=${tmp[0].question_id}`);
-//   let s = `<div class="buttons p-3 bg-white">
-//   <div
-//     class="row justify-content-around align-items-center"
-//     id="row"
-//   >
-//     <input
-//       type="button"
-//       value="PREV"
-//       onclick="fetcher('${tmp[0].question_id}')"
-//       class="border border-info rounded p-1 bg-white text-info font-weight-bold col-2"
-//       id="prev"
-//     />
-
-//     <input
-//       type="button"
-//       value="NEXT"
-//       onclick="next_btn('${tmp[0].question_id}')"
-//       class="btn btn-primary btn-success col-2 font-weight-bold"
-//       id="next"
-//     />
-//   </div>
-// </div>`;
-//   btns.innerHTML = s;
-// }
-
 async function previous_btn(id) {
-  console.log(id);
-  // let temp2 = await fetch(`htp://localhost:4321/previous?id=${id}`);
-  
+  let temp = await fetch(`http://localhost:4321/prev?id=${id}`);
+  let tmp = await temp.json();
+  await fetcher(
+    `/paging/?question_no=${tmp[0].question_id}&category_id=${tmp[0].category_id}`
+  );
+  let s = `<div
+    class="row justify-content-around align-items-center"
+    id="row"
+  >
+    <input
+      type="button"
+      value="PREV"
+      onclick="previous_btn('${tmp[0].question_id}')"
+      class="border border-info rounded p-1 bg-white text-info font-weight-bold col-2"
+      id="prev"
+    />
+
+    <input
+      type="button"
+      value="NEXT"
+      onclick="next_btn('${tmp[0].question_id}')"
+      class="btn btn-primary btn-success col-2 font-weight-bold"
+      id="next"
+    />
+  </div>`;
+  btns.innerHTML = s;
+}
+
+async function category_changer(e, a) {
+  let temp = await fetch(`/category?id=${e.id}`);
+  let ans = await temp.json();
+  // console.log('Data : -',ans);
+  let s = ` <div class="d-flex flex-row align-items-center question-title">
+                <h3 class="text-danger">Q.</h3>
+                <h5 class="mt-1 ml-2">${ans.data[0].question_text}</h5>
+              </div>
+              <div class="ans ml-2">
+                <label class="radio">
+                  <input type="radio" name="q-${ans.data[0].question_id}" value="${ans.data[0].option_a}">
+                  <span>${ans.data[0].option_a}</span>
+                </label>
+              </div>
+              <div class="ans ml-2">
+                <label class="radio">
+                  <input type="radio" name="q-${ans.data[0].question_id}" value="${ans.data[0].option_b}">
+                  <span>${ans.data[0].option_b}</span>
+                </label>
+              </div>
+              <div class="ans ml-2">
+                <label class="radio">
+                  <input type="radio" name="q-${ans.data[0].question_id}" value="${ans.data[0].option_c}">
+                  <span>${ans.data[0].option_c}</span>
+                </label>
+              </div>
+              <div class="ans ml-2">
+                <label class="radio">
+                  <input type="radio" name="q-${ans.data[0].question_id}" value="${ans.data[0].option_d}">
+                  <span>${ans.data[0].option_d}</span>
+                </label>
+              </div>`;
+  que.innerHTML = s;
+  let qn = `<span class= "que-no">${ans.data[0].question_id}</span>`;
+  que_no.innerHTML = qn;
+  let btn = `<div
+    class="row justify-content-around align-items-center"
+    id="row"
+  >
+    <input
+      type="button"
+      value="PREV"
+      onclick="previous_btn('${ans.data[0].question_id}')"
+      class="border border-info rounded p-1 bg-white text-info font-weight-bold col-2"
+      id="prev"
+    />
+
+    <input
+      type="button"
+      value="NEXT"
+      onclick="next_btn('${ans.data[0].question_id}')"
+      class="btn btn-primary btn-success col-2 font-weight-bold"
+      id="next"
+    />
+  </div>`;
+
+  btns.innerHTML = btn;
+  let page = "";
+  for (var i = 0; i < ans.category[0].no_of_question; i++) {
+    if ((i+1) === ans.question_no) {
+      page += `<span
+      class="selectlink m-1 btn"
+      onclick="fetcher('paging/?question_no=${ans.data[0].question_id + i}&category_id=${ans.category[0].category_id}')"
+      >${i+1}</span
+    >`;
+      continue;
+    }
+
+    page += ` <span
+      class="student m-1 btn"
+      onclick="fetcher('/paging/?question_no=${ans.data[0].question_id + i}&category_id=${ans.category[0].category_id}')"
+      >${i+1}</span
+    >`;
+  }
+
+  // console.log(page);s
+  document.getElementById("page").innerHTML = page;
 }
--- server.js
+++ server.js
@@ -18,12 +18,14 @@ const con = mysql2.createPool({
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.redirect("/exam");
+  // res.redirect("/exam");
+  res.render("quiz");
 });
+
 app.get("/exam", async (req, res) => {
   try {
     const question_no = 1,
-      category_id = 1; // Get the requested page number, default to 1 if not provided
+      category_id = req.query.category_id || 1; // Get the requested page number, default to 1 if not provided
     const question_per_page = 1; //* Limit || Number of questions to display per page
     const offset = (question_no - 1) * question_per_page;
     let no_of_question;
@@ -33,25 +35,19 @@ app.get("/exam", async (req, res) => {
       `SELECT exam_name,total_questions,exam_time,exam_access_code FROM exam WHERE exam_id = '${exam_id}'`
     );
     let [category] = await con.execute(
-      `SELECT category_name,category_id FROM category WHERE category_id = '${category_id}'`
+      `SELECT category_name,a.category_id,count(a.category_id) as no_of_question FROM category a, questions b WHERE a.category_id=b.category_id and a.category_id = '${category_id}'`
     );
-    let [records] = await con.execute(
-      `SELECT question_text,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id}`
-    );
-    let numberOfRecords = records.length;
-    no_of_question = Math.ceil(numberOfRecords / question_per_page);
 
     let [data] = await con.execute(
       `SELECT question_text,question_id,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id} LIMIT ${question_per_page} OFFSET ${offset}`
     );
-    // console.log(category);
+    console.log(category[0].no_of_question);
     if (data.length) {
       res.render("exam_question", {
         //Result,numberOfpage,page,numberOfperpage
         e: data[0],
         exam: exam,
         category: category,
-        no_of_question,
         question_no,
         question_per_page,
       });
@@ -61,30 +57,44 @@ app.get("/exam", async (req, res) => {
   }
 });
 
-app.get("/paging", async (req, res) => {
-  const category_id = 1,
-    question_no = req.query.question_no; // Get the requested page number, default to 1 if not provided
+app.get("/category", async (req, res) => {
+  let category_id = req.query.id;
+  let question_no = 1;
   const question_per_page = 1; //* Limit || Number of questions to display per page
-  const offset = (question_no - 1) * question_per_page;
-  let no_of_question;
+  let offset = (question_no - 1) * question_per_page;
   let exam_id = 1;
 
-  let [exam] = await con.execute(
-    `SELECT exam_name,total_questions,exam_time,exam_access_code FROM exam WHERE exam_id = '${exam_id}'`
-  );
   let [category] = await con.execute(
-    `SELECT category_name FROM category WHERE category_id = '${category_id}'`
+    `SELECT category_name,a.category_id,count(a.category_id) as no_of_question FROM category a, questions b WHERE a.category_id=b.category_id and a.category_id = '${category_id}'`
   );
-  let [records] = await con.execute(
-    `SELECT question_text,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id}`
-  );
-  let numberOfRecords = records.length;
-  no_of_question = Math.ceil(numberOfRecords / question_per_page);
 
   let [data] = await con.execute(
     `SELECT question_text,question_id,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id} LIMIT ${question_per_page} OFFSET ${offset}`
   );
-  res.json(data, no_of_question, question_no);
+  // console.log("category :- ", category);
+  res.json({data, category, question_no});
+});
+
+app.get("/paging", async (req, res) => {
+  const category_id = req.query.category_id || 1,
+    question_no = req.query.question_no + 1;
+  const question_per_page = 1; //* Limit || Number of questions to display per page
+  const offset = (question_no - 1) * question_per_page;
+  let no_of_question;
+  let exam_id = 1;
+  let [exam] = await con.execute(
+    `SELECT exam_name,total_questions,exam_time,exam_access_code FROM exam WHERE exam_id = '${exam_id}'`
+  );
+
+  let [category] = await con.execute(
+    `SELECT category_name,a.category_id,count(a.category_id) as no_of_question FROM category a, questions b WHERE a.category_id=b.category_id and a.category_id = '${category_id}'`
+  );
+
+  let [data] = await con.execute(
+    `SELECT question_text,question_id,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id} AND a.question_id=${req.query.question_no}`
+  );
+
+  res.json(data, category[0].no_of_question, question_no);
 });
 
 app.get("/next", async (req, res) => {
@@ -92,15 +102,15 @@ app.get("/next", async (req, res) => {
   let [question] = await con.execute(
     `SELECT * FROM questions WHERE question_id = ${id}`
   );
+  // console.log(question);
   res.json(question);
 });
-app.get("/previous", async (req, res) => {
+app.get("/prev", async (req, res) => {
   let id = parseInt(req.query.id) - 1;
-  console.log(`SELECT * FROM questions WHERE question_id = ${id}`);
   let [question] = await con.execute(
     `SELECT * FROM questions WHERE question_id = ${id}`
   );
-  // res.json(question);
+  res.json(question);
 });
 
 //? Taking student's response
@@ -110,7 +120,26 @@ app.post("/answers", async (req, res) => {
   // res.send(b);
 });
 
-app.get("/test", () => {});
+// * testing
+app.get("/quiz/:category_id", async (req, res) => {
+  const category_id = req.params.category_id,
+    currentPage = 1,
+    totalPages = 5;
+
+  try {
+    const query = `
+    SELECT question_text,option_a,option_b,option_c,option_d,a.category_id FROM exam_system.questions as a left join exam_system.category as b on a.category_id=b.category_id where a.category_id=${category_id}
+    `;
+    const [rows] = await con.execute(query, [category_id]);
+    // const questions = rows.map((row) => ({ question_text: row.question_text }));
+    // console.log(rows);
+    res.render("quiz", { questions: rows, currentPage, totalPages });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error");
+  }
+});
+
 const port = 4321;
 app.listen(port, () => {
   console.log("App started on port", port);
