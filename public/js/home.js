ff1670fe48c211cfb17c34ad3a23dc279849b7fb
Dhruvn-patel <dhruvpateldhruvpatel01669@gmail.com>
2023-03-23 12:23:01 +0530

    changed

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,15 @@
 let ischeckemail = true;
 let ischeckname1 = true, ischeckname2 = true;
 let isContact = true;
+let isaddress = true;
 var savebutton = document.getElementById('savebutton');
 var saveall = document.getElementById('totalSave');
 var readonly = true;
 var inputs = document.querySelectorAll('input');
 var firstname = document.getElementById('first_name').value;
-var id = document.getElementById('id_data').value;
 var email = document.getElementById('email').value;
 let contact = document.getElementById('contact');
-
+let address = document.getElementById('address').value;
 async function checkNameValid1(ele) {
     let input_val = ele.value;
     let err_mes = document.getElementById('err_name');
@@ -51,24 +51,6 @@ async function checkEmail(ele) {
 //     };
 // });
 
-async function checksubmit() {
-    if (ischeckemail && ischeckname1 && isContact) {
-        saveall.disabled = false;
-        saveall.style.cursor = 'pointer';
-        console.log(firstname, email)
-        const ans = await fetch('/profile_update', {
-            method: 'POST', headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                id, firstname, email, contact
-            })
-        });
-
-    }
-    else {
-        saveall.disabled = true;
-    }
-}
-
 
 async function checkContact(ele) {
     const contact_val = ele.value;
@@ -93,3 +75,42 @@ async function checkContact(ele) {
 
     checksubmit();
 }
+
+async function checkAddress(ele) {
+
+    let input_address = ele.value;
+    const addre_err = document.getElementById('addre_err');
+    if (input_address == "") {
+        address = input_address;
+        addre_err.innerHTML = 'Address field is empty';
+        isaddress = false;
+    }
+    else {
+        address = input_address;
+        addre_err.innerHTML = '';
+        isaddress = true;
+    }
+    checksubmit();
+}
+
+async function checksubmit() {
+    if (ischeckemail && ischeckname1 && isContact && isaddress) {
+
+        saveall.disabled = false;
+        saveall.style.cursor = 'pointer';
+        console.log(firstname, email, contact.value, address)
+        const ans = await fetch('/profile_update', {
+            method: 'POST', headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                firstname, email, contact: contact.value, address
+            })
+        });
+
+    }
+    else {
+        saveall.disabled = true;
+    }
+}
+
+
+
